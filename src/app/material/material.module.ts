bc1cd46b6df876f960c9225ce2787a098440a2e6
hodayazac <88848337+hodayazac@users.noreply.github.com>
2022-05-29 11:27:58 +0300

    open progGit

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+const modules:any=[]
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule,...modules],
+  exports: modules 
+   //fdgfdg
+})
+export class MaterialModule { }
