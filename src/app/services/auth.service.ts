c438fa3202ef0dccba1cd991f216167dcc7aedad
hodayazac <88848337+hodayazac@users.noreply.github.com>
2022-05-30 12:31:06 +0300

    aded basic auth

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,24 +1,39 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { AngularFirestore } from '@angular/fire/compat/firestore'
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/compat/firestore'
 import * as firebase from 'firebase/compat/app'
+import { User } from '../models/user.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(
-    private aft: AngularFirestore, private afAuth: AngularFireAuth) { }
+  constructor(private afs: AngularFirestore, private afAuth: AngularFireAuth) { }
      public signInWithGoogle(){
        this.authLogin(new firebase.default.auth.GoogleAuthProvider())
      }
-
-
-
     private authLogin(provider: firebase.default.auth.AuthProvider){
       return this.afAuth.signInWithPopup(provider).then(res=>{
-        console.log(res)
+        this.setUserData(res.user as User);
       })
+    }
+
+    private setUserData(user?:User): Promise<void> | void{
+      if(!user) return
+      const userRef: AngularFirestoreDocument<User> =this.afs.doc(
+        `users/${user.uid}`
+      )
+      const userData:User={
+        uid: user.uid,
+        email: user.email,
+        displayName: user.displayName,
+        photoURL: user.photoURL,
+      }
+      return userRef.set(userData,{
+        merge:true
+      });
+
+
     }
 }
